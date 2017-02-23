eb9d7b9afafb8c8af936fb8b83972f6d16cf9eb3
Leander Costa <leander@outlook.pt>
2017-02-23 12:07:07 +0000

    ADD

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <title>a particula</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1">      <link rel="stylesheet" href="assets/css/bootstrap.min.css"/>      <link rel="stylesheet" href="assets/css/style.css"/>	<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>	
+   </head>
+   <body>
+      <header id="header">
+         <div class="background">
+            <video autoplay loop>
+               <source src="assets/media/main.mp4" type="video/mp4"/>
+            </video>
+         </div>
+         <div class="overlay"></div>
+         <div class="content">
+            <div class="container">
+                     <div class="div-container">				        <div class="div-centro">							<div class="div-centro-content">								<div class="div-centro-texto-g">De la stratégie à l’environnement digital</div>								<!-- <div class="div-centro-texto-s">Création de site internet, application mobile, identité visuelle, conseil, accompagnement de projets</div> -->							</div>						</div>						<div class="div-logo"></div>
+                     </div> 
+            </div>
+         </div>
+      </header>
+   </body>
+</html>
